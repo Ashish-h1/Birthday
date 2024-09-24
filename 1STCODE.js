f86785018cdece5bc88d2e35e44b5c39bfd1d32e
Ashish-h1 <147300292+Ashish-h1@users.noreply.github.com>
2024-09-24 10:36:26 +0530

    Add files via upload

---
+++ 1STCODE.js
@@ -0,0 +1,30 @@
+// // console.log("Ashish");
+// // age = "24";
+// // console.log(age);
+
+// const profile ={
+//     name:"Shradhakhapra",
+//     posts:195,
+//     followers:"569k",
+//     following:4,
+//     isFollow:false,
+
+// };
+// console.log(profile);
+// let num=prompt("Enter a numnber");
+// if(num%5===0){
+//   console.log(num, "is a multiple of 5");
+// }
+// else{
+//     console.log(num, "iss not a multiple of 5");
+// }
+let marks=prompt("Enter marks");
+if(marks>=80 && marks<=100){
+    console.log("Grade:A");
+} else if(marks>=70 && marks<79){
+    console.log("Grade:B");
+}else if(marks>=60 && marks<69){
+    console.log("Grade:C");
+}else{
+    console.log("Grade:F");
+}
---
+++ www.java
@@ -0,0 +1,11 @@
+package portfolio;
+
+class www {
+
+    public static void main(String[] args) {
+        while (true) {
+            // code to be executed repeatedly
+            System.out.println("Infinite loop!");
+        }
+    }
+}
